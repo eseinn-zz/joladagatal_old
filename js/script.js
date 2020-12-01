4784e3d9e03b1a1844b51ee285cdf1a31fc5d410
Einar Smári Einarsson <einar.smari.einarsson@origo.is>
2020-12-01 06:31:20 +0000

    Refactor css and js into seperate files. Fix switch statement and container visibility toggler.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,142 @@
+const getTodayDate = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const formatedDate = day + '.' + (date.getMonth() + 1) + '.' + '2020';
+  document
+    .getElementById('todayDate')
+    .appendChild(document.createTextNode(formatedDate));
+};
+
+const getCountDownDays = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const daysLeft = Math.abs(24 - day);
+  const countDown =
+    daysLeft === 0 ? 'Gleðileg jól!' : daysLeft + ' dagar til jóla';
+  document
+    .getElementById('countDownDays')
+    .appendChild(document.createTextNode(countDown));
+};
+
+const showContentIfDecemeber = () => {
+  const date = new Date();
+  const month = date.getMonth();
+  if (month === 11) {
+    document.getElementById('content').style.display = 'block';
+  }
+};
+getTodayDate();
+getCountDownDays();
+showContentIfDecemeber();
+
 const onButtonClick = () => {
-  alert('ok');
+  document.getElementById('button').style.display = 'none';
+  getExerciseForToday();
+};
+
+const getExerciseForToday = () => {
+  const date = new Date();
+  const day = date.getDate();
+  let exercise = null;
+  console.log(day);
+  switch (day) {
+    case 1:
+      exercise = '50 burpees (brjóta upp í viðráðanleg sett)';
+      break;
+
+    case 2:
+      exercise = '5 mín. wall squat (t.d. 1 min x5)';
+      break;
+
+    case 3:
+      exercise = '10 burpees';
+      break;
+
+    case 4:
+      exercise = '20 burpees';
+      break;
+
+    case 5:
+      exercise = '10 burpees';
+      break;
+
+    case 6:
+      exercise = '20 burpees';
+      break;
+
+    case 7:
+      exercise = '10 burpees';
+      break;
+
+    case 8:
+      exercise = '20 burpees';
+      break;
+
+    case 9:
+      exercise = '10 burpees';
+      break;
+
+    case 10:
+      exercise = '20 burpees';
+      break;
+
+    case 11:
+      exercise = '10 burpees';
+      break;
+
+    case 12:
+      exercise = '20 burpees';
+      break;
+
+    case 13:
+      exercise = '10 burpees';
+      break;
+
+    case 14:
+      exercise = '20 burpees';
+      break;
+
+    case 15:
+      exercise = '10 burpees';
+      break;
+
+    case 16:
+      exercise = '20 burpees';
+      break;
+
+    case 17:
+      exercise = '10 burpees';
+      break;
+
+    case 18:
+      exercise = '20 burpees';
+      break;
+
+    case 19:
+      exercise = '10 burpees';
+      break;
+
+    case 20:
+      exercise = '20 burpees';
+      break;
+
+    case 21:
+      exercise = '10 burpees';
+      break;
+
+    case 22:
+      exercise = '20 burpees';
+      break;
+
+    case 23:
+      exercise = '20 burpees';
+      break;
+
+    case 24:
+      exercise = '20 burpees';
+      break;
+  }
+  console.log(exercise);
+  const containers = document.getElementById('exercise').children;
+  containers[1].appendChild(document.createTextNode(exercise));
 };
